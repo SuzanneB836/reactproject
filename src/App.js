ca7fa560c3839b223be026a6a390517296c16914
Suzanne Boon <6035697@mborijnland.nl>
2025-02-06 19:49:44 +0100

    pretty-fying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('/data.json')
+    fetch("/reactproject/data.json")
       .then((response) => response.json())
-      .then((jsonData) => setData(jsonData))
-      .catch((error) => console.error('Error loading JSON:', error));
+      .then((jsonData) => {
+        setData(jsonData);
+      })
+      .catch((error) => console.error("Error loading JSON:", error));
   }, []);
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>JSON Data Display</h1>
-      <ul>
-        {data.map((item) => (
-          <li key={item.id}>
-            <strong>{item.name}</strong> - {item.email}
-          </li>
-        ))}
-      </ul>
+    <div className="parent">
+      <div className="header">
+        <h1>JSON Data Display</h1>
+        <div className="data">
+          {data.map((item) => (
+            <div className="data-item" key={item.id}>
+              <strong>{item.color}</strong> - {item.value}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
